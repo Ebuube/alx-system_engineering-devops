49d6cb0fcff9e42d6c80355f108e57337ca1da0e
Ebuube <onwutaebubegideon1555@gmail.com>
2023-09-23 11:26:43 +0000

    Puppet file passed linting test

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Create a file using puppet
+file { 'school':
+  ensure  => 'present',
+  path    => '/tmp/school',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet'
+}
