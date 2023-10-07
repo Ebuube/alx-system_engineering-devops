3deff7846f6008331150da2aa72f21979ff0c98b
Ebuube <onwutaebubegideon1555@gmail.com>
2023-10-07 19:44:15 +0000

    Task: update Puppet manifest

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -9,20 +9,17 @@ exec { 'apt-update':
 }
 
 # Install nginx package
-package { $server:
+-> package { $server:
   ensure  => installed,
-  require => Exec['apt-update'],
 }
 
 # ensure service is running
-service { $server:
+-> service { $server:
   ensure  => running,
-  require => Package[$server],
 }
 
 # Set firewall
-exec {'set_firewall':
-  require  => Service[$server],
+-> exec {'set_firewall':
   command  => 'ufw allow "Nginx HTTP"',
   provider => 'shell',
 }
@@ -32,20 +29,18 @@ $user = 'ubuntu'
 $site = '/var/www/html'
 $home = "/home/${user}/work"
 
-file {'set_site_owner':
-  ensure  => directory,
-  path    => $site,
-  require => Service[$server],
-  owner   => $user,
-  group   => $user,
-  mode    => 'ugo=rwx',
+-> file {'set_site_owner':
+  ensure => directory,
+  path   => $site,
+  owner  => $user,
+  group  => $user,
+  mode   => 'ugo=rwx',
 }
 
 $index_dest = "${site}/index.html"
 # $index_src = "${home}/index.html"
-file { 'landing_page':
+-> file { 'landing_page':
   ensure  => present,
-  require => File['set_site_owner'],
   path    => $index_dest,
   content => "Hello World!\n",
   owner   => $user,
@@ -54,10 +49,9 @@ file { 'landing_page':
 
 $err_page_dest = "${site}/404.html"
 # $err_page_src = "${home}/404.html"
-file { 'error_page':
+-> file { 'error_page':
   ensure  => present,
   path    => $err_page_dest,
-  require => File['set_site_owner'],
   content => "Ceci n'est pas une page\n\n",
   owner   => $user,
   group   => $user,
@@ -65,20 +59,18 @@ file { 'error_page':
 
 # Set pages for website
 $dummy_file = "${home}/dummy_file"
-File {'set_pages':
+-> File {'set_pages':
   path    => $dummy_file,
   ensure  => absent,
-  require => [File['landing_page'], File['error_page']],
 }
 
 # Configure server
 $available_site = '/etc/nginx/sites-available/default'
 $enabled_site = '/etc/nginx/sites-enabled/default'
 # $config_src = "${home}/server_config_4"
-file { 'server_config':
+-> file { 'server_config':
   ensure  => present,
   path    => $available_site,
-  require => Service[$server],
   content => @(END_OF_CONTENT)
 # Configuration for server - ONWUTA EBUBE GIDEON
 server {
@@ -116,15 +108,13 @@ server {
   END_OF_CONTENT
 }
 
-file { 'enabled_site':
-  ensure  => link,
-  path    => $enabled_site,
-  target  => $available_site,
-  require => File['server_config']
+-> file { 'enabled_site':
+  ensure => link,
+  path   => $enabled_site,
+  target => $available_site,
 }
 
-exec { 'nginx_restart':
+-> exec { 'nginx_restart':
   command  => "service ${server} restart",
-  require  => [Service[$server], File['set_pages'], File['enabled_site']],
   provider => 'shell',
 }
