1d74d8db5b9e90a4494a7c50b6975d0e99a8ac2c
Ebuube <onwutaebubegideon1555@gmail.com>
2023-10-27 19:59:36 +0000

    modified script to properly assign values

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -13,23 +13,23 @@ if __name__ == "__main__":
     delim = '/'
 
     # user
-    url = api + delim + 'users'
-    users = get(url).json()
-
-    # todos for this user
-    url = api + delim + 'todos'
-    todos = get(url).json()
+    user_url = api + delim + 'users'
+    users = get(user_url).json()
+    # todos
+    todo_url = api + delim + 'todos'
+    todos = get(todo_url).json()
 
     # Create data
     user_data = {}
     for user in users:
         details = []
         for todo in todos:
-            task = {}
-            task['task'] = todo.get('title')
-            task['completed'] = todo.get('completed')
-            task['username'] = user.get('username')
-            details.append(task)
+            if todo.get('userId') == user.get('id'):
+                task = {}
+                task['task'] = todo.get('title')
+                task['completed'] = todo.get('completed')
+                task['username'] = user.get('username')
+                details.append(task)
         user_data[user.get('id')] = details
 
     """
