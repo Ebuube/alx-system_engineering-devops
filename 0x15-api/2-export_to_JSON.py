82f2b6064cffb8144d81e33f9faa9a73420073e2
Ebuube <onwutaebubegideon1555@gmail.com>
2023-10-27 19:31:08 +0000

    Updated gitignore and modified script to use objects

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -24,35 +24,20 @@ if __name__ == "__main__":
     url = api + delim + 'users' + delim + emp_id + delim + 'todos'
     todos = get(url).json()
 
-    '''
     # Create data
     user_data = {}
     user_data[emp_id] = []
     for todo in todos:
-        if todo.get('completed') is True:
-            status = 'true'
-        else:
-            status = 'false'
-            task = {"task": todo.get('title'), "completed": status,
-                    "username": user.get('username')}
-            user_data[emp_id].append()
-    '''
+        task = {}
+        task['task'] = todo.get('title')
+        task['completed'] = todo.get('completed')
+        task['username'] = user.get('username')
+        user_data[emp_id].append(task)
 
     """
     Write information to a file
     """
     file_fmt = ".json"
     filename = emp_id + file_fmt
-    with open(filename, 'w') as f:
-        f.write('{{"{}": ['.format(emp_id))  # opening
-        for todo in todos:
-            if todo.get('completed') is True:
-                status = 'true'
-            else:
-                status = 'false'
-            msg = '{{"task": "{}", "completed": {}, "username": {}}}'.format(
-                    todo.get('title'), status, user.get('username'))
-            f.write(msg)
-            if todo != todos[-1]:
-                f.write(", ")
-        f.write("]}")
+    with open(filename, 'w') as user_file:
+        json.dump(user_data, user_file)
