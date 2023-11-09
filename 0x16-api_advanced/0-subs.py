c04bba4e81594d3039e4e0be6af76b7c40089a36
Ebuube <onwutaebubegideon1555@gmail.com>
2023-11-09 09:22:13 +0000

    Task 0: How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import requests
+from sys import argv
+import json
+
+
+def number_of_subscribers(subreddit):
+    """
+    Get the number of subscribers to a given subreddit
+    """
+    root = 'https://www.reddit.com/'
+    end_point = '/r/{}/about.json'.format(subreddit)
+    url = str(root) + str(end_point)
+    user_agent = """Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 \
+Safari/537.36 Avast/118.0.0.0"""
+    headers = {
+            "User-Agent": user_agent
+            }
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        subscribers = response.json().get('data').get('subscribers')
+        if subscribers is not None:
+            return subscribers
+    else:
+        return 0
