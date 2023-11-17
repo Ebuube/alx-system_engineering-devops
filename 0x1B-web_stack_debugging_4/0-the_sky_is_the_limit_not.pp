34f6efc5f93c28352cb2e4eb12b7e802ff17412e
Ebuube <onwutaebubegideon1555@gmail.com>
2023-11-17 14:36:50 +0000

    Task 0: Sky is the limit, let us bring that limit higher

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,14 @@
 # Adjust ulimit value
-exec { 'adjust ulimit':
-  command  => "sudo sed -i 's/-n .*\"/-n 4096\"/g' /etc/default/nginx",
-  provider => 'shell'
+package { 'nginx':
+  ensure => installed
 }
 
-exec { 'restart nginx':
-  command  => 'sudo service nginx restart',
-  provider => 'shell'
+-> exec { 'adjust ulimit':
+  command  => "sudo sed -i 's/-n .*\"/-n 4096\"/g' /etc/default/nginx",
+  provider => 'shell',
+}
+
+-> exec {'stop nginx':
+  command  => 'sudo /usr/sbin/service nginx restart',
+  provider => 'shell',
 }
